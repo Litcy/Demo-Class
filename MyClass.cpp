3ab33e67b30fb0eb179bc2ca37bd89458be166f0
litcy <Litcy@users.noreply.github.com>
2016-09-23 20:59:50 +0000

    Filled in missing parts of implementation

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,1 +1,37 @@
+void myClass::Myclass()
+{
+    int num;
+    string room;
+    bool fun;
+}
 
+void myClass::setNum(int n)
+{
+     num = n;
+}
+
+int myClass::getNum()
+{
+     return num;
+}
+
+void myClass::setRoom(int r)
+{
+     room = r;
+}
+
+int myClass::getRoom()
+{
+     return room;
+}
+
+
+void myClass::setFun(int f)
+{
+     fun = f;
+}
+
+bool myClass::getFun()
+{
+     return fun;
+}
