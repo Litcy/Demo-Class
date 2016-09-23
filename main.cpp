6a19517c57d87b2fe29d4e1c15e7995454b2b05f
litcy <Litcy@users.noreply.github.com>
2016-09-23 21:10:35 +0000

    Filled in missing parts of client

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 //complete me
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 
 int main()
 {
+    myClass myVar;
+    string str;
+    int var = 10;
+    bool bo = true;
+
+    cout << " Var= " << endl;
+    
+    cout << "Enter a string" << endl;
+    cin >> str;
+
+    myVar.setNum(var);
+    myVar.setRoom(str);
+    myVar.setFun(var);
+
+    cout << myVar.getNum() << endl;
+    cout << myVar.getRoom(str) << endl;
+    cout << myVar.getFun(var) << endl;
+
+    system("PAUSE");
+    return 0;
 
 }
